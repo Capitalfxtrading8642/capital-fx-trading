444c8342d88c0e00033accc8e9be50a2f8fdd873
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2022-12-06 03:51:10 +0000

    Initial commit

---
+++ src/backend/___spi___/ecom-catalog/comot/comot-config.js
@@ -0,0 +1,4 @@
+/** @returns {import('interfaces-ecommerce-v1-catalog-provider').CatalogSPIConfig */
+export function getConfig() {
+  throw new Error('getConfig was not implemented');
+}
---
+++ src/pages/masterPage.js
@@ -0,0 +1,11 @@
+<a class="navbar-brand href="https://capitalfx-trading.com
+>
+ <a href="login" class="font-14 whitecolor hover-default"><i class="fas fa-user mr-2"></i>Login</a> <
+div class = "bg-transparent d-inline-block d-xs-none position-relative mr-2 ml-2" > </div>
+ <a href = "register" class = "font-14 whitecolor hover-default" > <i class="fas fa-lock mr-2"></i>Register < /a> < /
+    div > <
+    /div> <
+    /div> <
+    /div> <
+    nav class = "navbar navbar-expand-lg transparent-bg darkcolor static-nav" >
+    <div class="container bgnone nav-cont">
